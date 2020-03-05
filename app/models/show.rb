529ae6cd9776c8aa440394a5e91e692bec5768af
Damely Tineo <damely.tineo07@gmail.com>
2020-03-05 05:00:44 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.post_popular
-    Show.maximum(:rating).name
+    Show.find_by(:name).order('rating DESC').first
   end
   
   def self.lowest_rating
